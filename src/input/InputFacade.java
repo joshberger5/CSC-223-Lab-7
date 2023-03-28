f9bf2900836b1b7637a54e01ac6fe5c3cc6f503c
SLuckLeonard <122399848+SLuckLeonard@users.noreply.github.com>
2023-03-28 16:23:13 -0400

    Initial InputFacade

--- src/input/InputFacade.java
+++ src/input/InputFacade.java
@@ -1,7 +1,5 @@
 package input;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.AbstractMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
@@ -10,43 +8,57 @@ import java.util.Set;
 import geometry_objects.points.Point;
 import geometry_objects.points.PointDatabase;
 import geometry_objects.Segment;
+import input.builder.DefaultBuilder;
 import input.builder.GeometryBuilder;
 import input.components.ComponentNode;
 import input.components.FigureNode;
 import input.components.point.PointNode;
+import input.components.point.PointNodeDatabase;
 import input.components.segment.SegmentNode;
+import input.components.segment.SegmentNodeDatabase;
 import input.parser.JSONParser;
 
-public class InputFacade
-{
+public class InputFacade {
+	
 	/**
 	 * A utility method to acquire a figure from the given JSON file:
 	 *     Constructs a parser
-	 *     Acquries an input file string.
+	 *     Acquires an input file string.
 	 *     Parses the file.
-     *
+	 *
 	 * @param filepath -- the path/name defining the input file
 	 * @return a FigureNode object corresponding to the input file.
 	 */
-	public static FigureNode extractFigure(String filepath)
-	{
-        // TODO
+	public static FigureNode extractFigure(String filepath) {
+		String input = filepath;
+		JSONParser parser = new JSONParser(new DefaultBuilder());
+		return (FigureNode) parser.parse(input);
 	}
-	
+
 	/**
-	 * 1) Convert the PointNode and SegmentNode objects to a Point and Segment objects 
+	 * 1) Convert the PointNode and SegmentNode objects to a Point and Segment objects
 	 *    (those classes have more meaningful, geometric functionality).
 	 * 2) Return the points and segments as a pair.
-     *
+	 *
 	 * @param fig -- a populated FigureNode object corresponding to a geometry figure
 	 * @return a point database and a set of segments
 	 */
-	public static Map.Entry<PointDatabase, Set<Segment>> toGeometryRepresentation(FigureNode fig)
-	{
-		// TODO
+	public static Map.Entry<PointDatabase, Set<Segment>> toGeometryRepresentation(FigureNode fig) {
+	    PointDatabase pointDB = new PointDatabase();
+	    Set<Segment> segments = new LinkedHashSet<>();
+
+	    for (PointNode pointNode : fig.getPointsDatabase().getAllPoints()) {
+	        pointDB.put(pointNode.getName(), pointNode.getX(), pointNode.getY());
+	    }
+
+	    for (SegmentNode segmentNode : fig.getSegments().asSegmentList()) {
+	        Point start = pointDB.getPoint(segmentNode.getPoint1().getName());
+	        Point end = pointDB.getPoint(segmentNode.getPoint2().getName());
+	        Segment segment = new Segment(start, end);
+	        segments.add(segment);
+	    }
+
+	    return new AbstractMap.SimpleEntry<>(pointDB, segments);
 	}
 
-    //	
-	// TODO: implement other support methods to facilitate the toGeometryRepresentation method
-	//
 }
