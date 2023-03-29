ce7051713d4f0117c506654cc5bc03cada79952a
SLuckLeonard <122399848+SLuckLeonard@users.noreply.github.com>
2023-03-29 14:43:10 -0400

    empty test classes

---
+++ test/input/InputFacadeTest.java
@@ -0,0 +1,19 @@
+package input;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class InputFacadeTest {
+
+	@Test
+	void testExtractFigure() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToGeometryRepresentation() {
+		fail("Not yet implemented");
+	}
+
+}
