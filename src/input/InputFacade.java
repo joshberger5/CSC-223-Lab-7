350281fb845be86aa869698713a2eb8e51de7401
SLuckLeonard <122399848+SLuckLeonard@users.noreply.github.com>
2023-04-03 12:06:11 -0400

    final

--- src/input/InputFacade.java
+++ src/input/InputFacade.java
@@ -8,55 +8,59 @@ import java.util.Set;
 import geometry_objects.points.Point;
 import geometry_objects.points.PointDatabase;
 import geometry_objects.Segment;
-import input.builder.DefaultBuilder;
 import input.builder.GeometryBuilder;
-import input.components.ComponentNode;
 import input.components.FigureNode;
 import input.components.point.PointNode;
 import input.components.segment.SegmentNode;
 import input.parser.JSONParser;
 
+/**
+ * Helpers for loading geometry figures from input JSON files,
+ * and converting them from their input formats to geometry
+ * representations.
+ *
+ * @author Sam Luck-Leonard
+ * @date 4/3/2023
+ */
 public class InputFacade {
-	
-	/**
-	 * A utility method to acquire a figure from the given JSON file:
-	 *     Constructs a parser
-	 *     Acquires an input file string.
-	 *     Parses the file.
-	 *
-	 * @param filepath -- the path/name defining the input file
-	 * @return a FigureNode object corresponding to the input file.
-	 */
-	public static FigureNode extractFigure(String filepath) {
-		String input = filepath;
-		JSONParser parser = new JSONParser(new DefaultBuilder());
-		return (FigureNode) parser.parse(input);
-	}
+    /**
+     * A utility method to acquire a figure from the given JSON file:
+     * Constructs a parser
+     * Acquires an input file string.
+     * Parses the file.
+     *
+     * @param filepath -- the path/name defining the input file
+     * @return a FigureNode object corresponding to the input file.
+     */
+    public static FigureNode extractFigure(String filepath) {
+        String input = utilities.io.FileUtilities.readFileFilterComments(filepath);
+        JSONParser parser = new JSONParser(new GeometryBuilder());
+        return (FigureNode) parser.parse(input);
+    }
 
-	/**
-	 * 1) Convert the PointNode and SegmentNode objects to a Point and Segment objects
-	 *    (those classes have more meaningful, geometric functionality).
-	 * 2) Return the points and segments as a pair.
-	 *
-	 * @param fig -- a populated FigureNode object corresponding to a geometry figure
-	 * @return a point database and a set of segments
-	 */
-	public static Map.Entry<PointDatabase, Set<Segment>> toGeometryRepresentation(FigureNode fig) {
-	    PointDatabase pointDB = new PointDatabase();
-	    Set<Segment> segments = new LinkedHashSet<>();
+    /**
+     * Convert the PointNode and SegmentNode objects to a Point and Segment objects
+     * (those classes have more meaningful, geometric functionality).
+     * Return the points and segments as a pair.
+     *
+     * @param fig -- a populated FigureNode object corresponding to a geometry figure
+     * @return a point database and a set of segments
+     */
+    public static Map.Entry<PointDatabase, Set<Segment>> toGeometryRepresentation(FigureNode fig) {
+        PointDatabase pointDB = new PointDatabase();
+        Set<Segment> segments = new LinkedHashSet<>();
 
-	    for (PointNode pointNode : fig.getPointsDatabase().getAllPoints()) {
-	        pointDB.put(pointNode.getName(), pointNode.getX(), pointNode.getY());
-	    }
+        for (PointNode pointNode : fig.getPointsDatabase().getAllPoints()) {
+            pointDB.put(pointNode.getName(), pointNode.getX(), pointNode.getY());
+        }
 
-	    for (SegmentNode segmentNode : fig.getSegments().asSegmentList()) {
-	        Point start = pointDB.getPoint(segmentNode.getPoint1().getName());
-	        Point end = pointDB.getPoint(segmentNode.getPoint2().getName());
-	        Segment segment = new Segment(start, end);
-	        segments.add(segment);
-	    }
+        for (SegmentNode segmentNode : fig.getSegments().asUniqueSegmentList()) {
+            Point start = pointDB.getPoint(segmentNode.getPoint1().getName());
+            Point end = pointDB.getPoint(segmentNode.getPoint2().getName());
+            Segment segment = new Segment(start, end);
+            segments.add(segment);
+        }
 
-	    return new AbstractMap.SimpleEntry<>(pointDB, segments);
-	}
-
+        return new AbstractMap.SimpleEntry<>(pointDB, segments);
+    }
 }
--- test/input/InputFacadeTest.java
+++ test/input/InputFacadeTest.java
@@ -4,37 +4,32 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Stream;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import geometry_objects.Segment;
 import geometry_objects.points.PointDatabase;
-import input.builder.GeometryBuilder;
-import input.components.ComponentNode;
 import input.components.FigureNode;
-import input.parser.JSONParser;
-import utilities.io.FileUtilities;
 
 public class InputFacadeTest {
 
     /**
-     * Gets a stream of test JSON files
+     * Provides a list of test JSON file names
      *
-     * @return a Stream containing a list of file names in the testFiles folder
+     * @return a list of file names in the testFiles folder
      */
-    public static Stream<String> provideStringsForInputFacade() {
-        return Stream.of("collinear_line_segments.json", "crisscross_square.json", "fully_connected_irregular_polygon.json");
-    }
-
-    private static String getFigureString(String filename) {
-        return FileUtilities.readFileFilterComments(filename);
+    public static Object[][] provideInputFileNames() {
+        return new Object[][] {
+            { "collinear_line_segments.json" },
+            { "crisscross_square.json" },
+            { "fully_connected_irregular_polygon.json" }
+        };
     }
 
     @ParameterizedTest
-    @MethodSource("provideStringsForInputFacade")
-    public void inputFacadeExtractFigureTest(String filename) {
+    @MethodSource("provideInputFileNames")
+    public void testExtractFigure(String filename) {
         FigureNode n = InputFacade.extractFigure(filename);
 
         // make sure the FigureNode has data
@@ -44,19 +39,15 @@ public class InputFacadeTest {
     }
 
     @ParameterizedTest
-    @MethodSource("provideStringsForInputFacade")
-    public void inputFacadeToGeometryRepresentationTest(String filename) {
-        String figureStr = getFigureString(filename);
-        JSONParser parser = new JSONParser(new GeometryBuilder());
-        ComponentNode rootNode = parser.parse(figureStr);
-
+    @MethodSource("provideInputFileNames")
+    public void testToGeometryRepresentation(String filename) {
         FigureNode n = InputFacade.extractFigure(filename);
 
         Map.Entry<PointDatabase, Set<Segment>> geomEntry = InputFacade.toGeometryRepresentation(n);
         PointDatabase db = geomEntry.getKey();
         Set<Segment> segments = geomEntry.getValue();
 
-        // make sure the geometry representation is populated
+        // make sure the geometry representation is filled
         assertFalse(db.getPoints().isEmpty());
         assertFalse(segments.isEmpty());
     }
